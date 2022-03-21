3ab1001c37815529673ed2a14d37489be2f50be8
keinam53 <80219223+keinam53@users.noreply.github.com>
2022-03-21 20:09:04 +0100

    Delete 3_Datetime_interwaly_czasowe directory

--- 3_Datetime_interwaly_czasowe/4_Datetime_w_ISO/homework/main.py
+++
@@ -1,9 +0,0 @@
-from new_movies.actions import cinema_movies_schedule
-
-
-def run_example():
-    cinema_movies_schedule()
-
-
-if __name__ == "__main__":
-    run_example()
--- 3_Datetime_interwaly_czasowe/7_Parsowanie_daty_czasu/Przyklad/book_book/user.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from book_book.datetime_preferences import DatetimePreference
-
-
-@dataclass
-class User:
-    login: str
-    datetime_preferences: DatetimePreference = DatetimePreference.EUROPE
